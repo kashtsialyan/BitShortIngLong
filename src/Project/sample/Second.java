aded1c438e2133190b88a59b8df39ede4c605294
Antoshechkaa <kashtelyan.anton@gmail.com>
2022-07-11 12:29:24 +0400

    init 3

---
+++ src/Project/sample/Second.java
@@ -0,0 +1,25 @@
+package Project.sample;
+
+public class Second {
+
+    public static void main(String[] args) {
+
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Min = " + myMaxFloatValue);
+        System.out.println("Max = " + myMinFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Min = " + myMaxDoubleValue);
+        System.out.println("Max = " + myMinDoubleValue);
+
+        int myIntValue = 5;
+        float myFloatValue = 5f;
+        double myDoubleValue = 5d;
+        System.out.println("MyIntValue= " + myIntValue);
+        System.out.println("MyFloatValue= " + myFloatValue);
+        System.out.println("MyDoublealue= " + myDoubleValue);
+
+    }
+}
