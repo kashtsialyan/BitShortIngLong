5c41208020f165c6be892927916487b9b4b63ffd
Antoshechkaa <kashtelyan.anton@gmail.com>
2022-07-11 13:04:09 +0400

    init 5

--- src/Project/sample/Third.java
+++ src/Project/sample/Third.java
@@ -1,7 +1,13 @@
 package Project.sample;
 
 public class Third {
+
     public static void main(String[] args) {
+
+        char myChar = 'D';
+        char myUnicodeChar = '\u00A9';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
 
     }
 }
