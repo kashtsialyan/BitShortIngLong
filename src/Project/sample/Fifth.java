0e50a1ad95b2ef3ef5507f45fe4bb7b0c1283052
Antoshechkaa <kashtelyan.anton@gmail.com>
2022-07-11 18:25:29 +0400

    init 6

--- src/Project/sample/Fifth.java
+++ src/Project/sample/Fifth.java
@@ -25,5 +25,28 @@ public class Fifth {
         result++; // 1 + 1 = 2
         System.out.println("1 + 1 = " + result);
 
+        result--; // 2 - 1 = 1
+        System.out.println("2 - 1 = " + result);
+
+        // result = result +2
+        result += 2; // 1 + 2 = 3
+        System.out.println("1 + 2 = " + result);
+
+        result *= 10; // 3 * 10 = 30
+        System.out.println("3 * 10 = " + result);
+
+        result /= 3; // 30 / 3 = 10
+        System.out.println("30 / 3 = " + result);
+
+        result -= 2; // 10 - 2 = 8
+        System.out.println(("10 - 2 = "+ result));
+
+        boolean isAlien = false;
+        if (isAlien == false)
+            System.out.println("it is not an alien!");
+        else System.out.println("no");
+
+
+
     }
 }
