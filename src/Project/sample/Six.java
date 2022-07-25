90e1c316b0cc2814df475632d17ebb16aab22a07
Antoshechkaa <kashtelyan.anton@gmail.com>
2022-07-25 10:49:07 +0300

    init 6

---
+++ src/Project/sample/Six.java
@@ -0,0 +1,27 @@
+package Project.sample;
+
+public class Six {
+
+    public static void main(String[] args) {
+
+        double kilometers = (100 * 1.609344);
+        int highScore = 50;
+        if (highScore == 50){
+            System.out.println("This is an expression" + highScore);
+
+        }
+
+
+
+
+
+        int score = 100;
+        if (score > 99) {
+            System.out.println("You got the high score!");
+            score = 0;
+        }
+
+
+
+    }
+}
